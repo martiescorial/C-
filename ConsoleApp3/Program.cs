72d2f6dff702787ea7ff8d94ef07042b68bc315f
Martina <martinaescorial9@gmail.com>
2021-06-12 21:38:06 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine(x);
+            int y = 5;
+            Console.WriteLine(y);
+            Console.WriteLine("Suma = {0}", x + y);
+            Console.WriteLine("Resta = {0}", x - y);
+            Console.WriteLine("Multiplicación = {0}", x * y);
+            Console.WriteLine("División = {0}", x / y);
+            Console.WriteLine("Módulos = {0}", x % y);
+        }
+    }
+}
